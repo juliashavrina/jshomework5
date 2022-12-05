a0c4fee159d546ffdd84629ce0c2eaf325469e79
juliashavrina <107791952+juliashavrina@users.noreply.github.com>
2022-12-05 21:26:23 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 // Задание 1
-const numbers = {
-        keyin1: 1,
-        keyin2: 2,
-        keyin3: 3,
-        keyin4: 4,
-        keyin5: 5,
-        keyin6: 6,
-        keyin7: 7,
-    }
+// const numbers = {
+//         keyin1: 1,
+//         keyin2: 2,
+//         keyin3: 3,
+//         keyin4: 4,
+//         keyin5: 5,
+//         keyin6: 6,
+//         keyin7: 7,
+//     }
     // Два варианта
     // 1 вариант
     // for (const key in numbers) {
